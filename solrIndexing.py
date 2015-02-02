9b55ae875c06c5ae4002b5de7df40bff77e680fc
georgeno <georgeno@post.jce.ac.il>
2015-02-02 05:59:28 -0800

    Delete solrIndexing.py

--- solrIndexing.py
+++
@@ -1,8 +0,0 @@
-import json
-import codecs
-
-in_file=codecs.open('C:/Users/George/Desktop/1.json','r',encoding='utf-8')
-new_dict=json.load(in_file)
-in_file.close()
-print(new_dict) # testing purposes.
-# we can do a normal open to test on english file.
